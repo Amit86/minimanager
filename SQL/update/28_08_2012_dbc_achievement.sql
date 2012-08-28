5565f4260a73d45bc2a8a5d7650091a65ce917b7
Amit Local Server <Amit86@gmail.com>
2012-08-28 19:47:39 +0300

    Add some missing achievements to DBC

---
+++ SQL/update/28_08_2012_dbc_achievement.sql
@@ -0,0 +1,10 @@
+DELETE FROM `dbc_achievement` WHERE id IN(4815,4816,4817,4818,4820,4821,4822,4823);
+INSERT INTO `dbc_achievement` (`id`, `name01`, `description01`, `categoryid`, `rewpoints`, `order`, `flags`, `field_42`, `rewarddesc01`) VALUES
+('4815','The Twilight Destroyer (25 player)','Defeat Halion in The Ruby Sanctum in 25-player mode.','14923','10','35','0','164',''),
+('4816','Heroic: The Twilight Destroyer (25 player)','Defeat Halion in The Ruby Sanctum in 25-player Heroic mode.','14923','10','36','0','164',''),
+('4817','The Twilight Destroyer (10 player)','Defeat Halion in The Ruby Sanctum in 10-player mode.','14922','10','35','0','164',''),
+('4818','Heroic: The Twilight Destroyer (10 player)','Defeat Halion in The Ruby Sanctum in 10-player Heroic mode.','14922','10','36','0','164',''),
+('4820','Halion kills (Ruby Sanctum 25 player)','Halion kills (Ruby Sanctum 25 player)','15062','0','69','1','1',''),
+('4821','Halion kills (Ruby Sanctum 10 player)','Halion kills (Ruby Sanctum 10 player)','15062','0','67','1','1',''),
+('4822','Halion kills (Heroic Ruby Sanctum 10 player)','Halion kills (Heroic Ruby Sanctum 10 player)','15062','0','68','1','1',''),
+('4823','Halion kills (Heroic Ruby Sanctum 25 player)','Halion kills (Heroic Ruby Sanctum 25 player)','15062','0','70','1','1','');
